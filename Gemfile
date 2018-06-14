f365b2282b8fce3fa9020bdc7f1f3d575f7d0fe7
mfh221290 <mfh_221290@hotmail.com>
2018-06-13 17:34:42 -0700

    Files added

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+ruby "2.5.1"
+gem 'sinatra', '~> 1.4'
+gem 'yaml_store'
+gem 'shotgun'
---
+++ principal.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+require './estacion.rb'
+require './viaje.rb'
+require './usuario.rb'
+
+get "/" do
+	erb :home
+end
+
+get "/usuario/nuevo" do
+	erb :usuarionew		
+end
+
+get "/usuario/modificar" do
+	erb :modificarusuario
+end
+
+get "/estacion/nuevo" do	
+	erb :estacionnew
+end
+
+get "/estacion/modificar" do  
+	erb :modificarestacion
+end
+
+get "/viaje/nuevo" do  
+	erb :viajenew
+end
+
+	
