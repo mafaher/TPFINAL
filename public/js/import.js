df81a816bbd74b1a13160a5bb1b27c113aadd7aa
mfh221290 <mfh_221290@hotmail.com>
2018-06-18 09:35:52 -0700

    forgot to include files

---
+++ public/js/import.js
@@ -0,0 +1,58 @@
+	
+	    $("#exampleFormControlFile1").change(function() {
+        var reader=new FileReader();
+
+
+        
+        var objecttoread=$("#exampleFormControlFile1").prop("files");
+
+        var file= objecttoread[0];
+        
+    	reader.onload = function () {
+    	var data=reader.result
+    	var tableresponsive=$("<div></div>").attr("class","table-responsive");
+		var table=$("<table></table>").attr("class","table");
+        var theader=$("<thead></thead>");
+        var tbody=$("<tbody></tbody>");
+        data=data.split("\n");
+
+        for (var i in data){
+        	data[i]=data[i].split(";");
+        }
+
+		var trheader=$("<tr></tr>");
+
+        for (var head in data[0]){
+        	var th=$("<th></th>").text(data[0][head]);
+        	trheader.append(th);
+        }
+
+        theader.append(trheader);
+
+        table.append(theader);
+
+        for (var place in data){
+        	if (place > 0){
+        	var tr=$("<tr></tr>");
+        	for (var body in data[place]){
+        		var td=$("<td></td>").text(data[place][body]);
+        		tr.append(td);
+        	}
+        	tbody.append(tr);
+      	  }
+      	}
+        table.append(tbody);
+
+        tableresponsive.append(table);
+        $("#table").html("");
+        $("#table").append(tableresponsive);
+
+   		 }
+
+
+    	reader.onerror = function (evt) {
+       console.log("error reading file");
+   		 }
+   		 reader.readAsText(file);
+		}
+    );
